f58323d4e3777f5e592cf67dad921b62b5d453c3
Valery <nicemrlexx@live.ru>
2025-03-04 12:40:18 +0300

    list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -2,12 +2,17 @@ package hw04lrucache
 
 type List interface {
 	Len() int
+
 	Front() *ListItem
 	Back() *ListItem
+
 	PushFront(v interface{}) *ListItem
 	PushBack(v interface{}) *ListItem
+
 	Remove(i *ListItem)
 	MoveToFront(i *ListItem)
+
+	Clear()
 }
 
 type ListItem struct {
@@ -17,10 +22,95 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len int
+
+	front *ListItem
+	back  *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (list list) Len() int {
+	return list.len
+}
+
+func (list list) Front() *ListItem {
+	return list.front
+}
+
+func (list list) Back() *ListItem {
+	return list.back
+}
+
+func (list *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{Value: v, Next: nil, Prev: nil}
+
+	switch list.len {
+	case 0:
+		list.front = item
+		list.back = item
+	default:
+		list.front.Prev = item
+		item.Next = list.front
+		list.front = item
+	}
+
+	list.len++
+	return list.front
+}
+
+func (list *list) PushBack(v interface{}) *ListItem {
+	if list.len == 0 {
+		return list.PushFront(v)
+	}
+
+	item := &ListItem{Value: v, Next: nil, Prev: nil}
+
+	item.Prev = list.back
+	list.back.Next = item
+	list.back = item
+
+	list.len++
+	return list.front
+}
+
+func (list *list) Remove(i *ListItem) {
+	if list.len == 0 {
+		return
+	}
+
+	switch {
+	case i.Prev == nil && i.Next == nil:
+		list.back, list.front = nil, nil
+
+	case i.Prev != nil && i.Next != nil:
+		i.Prev.Next, i.Next.Prev = i.Next, i.Prev
+
+	case i.Prev != nil:
+		i.Prev.Next = nil
+		list.back = i.Prev
+
+	case i.Next != nil:
+		i.Next.Prev = nil
+		list.front = i.Next
+	}
+
+	list.len--
+}
+
+func (list *list) MoveToFront(i *ListItem) {
+	if list.len > 1 {
+		list.Remove(i)
+		i.Next = list.front
+		list.front = i
+		i.Prev = nil
+	}
+}
+
+func (list *list) Clear() {
+	list.front = nil
+	list.back = nil
+	list.len = 0
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,71 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("front and back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // 10
+		require.Equal(t, true, l.Front() == l.Back())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+	})
+
+	t.Run("move to front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+
+		front := l.Front()
+		l.MoveToFront(front)
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+
+		l.PushBack(20)
+		l.PushBack(30)
+		l.PushBack(40)
+		l.PushBack(50) // 10, 20, 30, 40, 50
+
+		front = l.Front()
+		l.MoveToFront(front) // [10, 20, 30, 40, 50]
+		require.Equal(t, 10, l.Front().Value)
+
+		back := l.Back()
+		l.MoveToFront(back) // [50, 10, 20, 30, 40]
+		require.Equal(t, 50, l.Front().Value)
+	})
+
+	t.Run("remove", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10)
+		l.PushBack(20)
+		l.PushBack(30)
+		l.PushBack(40)
+		l.PushBack(50) // 10, 20, 30, 40, 50
+
+		back := l.Back()
+		l.Remove(back) // [10, 20, 30, 40]
+		require.Equal(t, 40, l.Back().Value)
+		require.Equal(t, 4, l.Len())
+
+		front := l.Front()
+		l.Remove(front) // [20, 30, 40]
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 3, l.Len())
+
+		l.Remove(l.Front()) // [30, 40]
+		l.Remove(l.Front()) // [40]
+		l.Remove(l.Front()) // []
+		l.Remove(l.Front()) // []
+		require.Equal(t, 0, l.Len())
+
+		elems := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			elems = append(elems, i.Value.(int))
+		}
+		require.Equal(t, []int{}, elems)
+	})
 }
