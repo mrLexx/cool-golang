243c3ae23ea991c3fb9cb34c79607b5a44b939ff
Valery <nicemrlexx@live.ru>
2025-03-04 12:40:26 +0300

    cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,14 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type lruCacheItem struct {
+	k Key
+	v interface{}
 }
 
 func NewCache(capacity int) Cache {
@@ -22,4 +25,37 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	_, ok := c.items[key]
+
+	if !ok && c.queue.Len() >= c.capacity {
+		last := c.queue.Back()
+
+		delete(c.items, last.Value.(lruCacheItem).k)
+		c.queue.Remove(last)
+	}
+
+	v := lruCacheItem{k: key, v: value}
+	elem := c.queue.PushFront(v)
+	c.items[key] = elem
+
+	return ok
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	elem, ok := c.items[key]
+
+	if !ok {
+		return nil, false
+	}
+	c.queue.PushFront(elem)
+
+	return elem.Value.(lruCacheItem).v, true
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.queue.Clear()
 }
