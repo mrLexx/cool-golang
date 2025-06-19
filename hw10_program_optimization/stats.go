5cd603639c191f09c629809e9a6f924ec28ed32b
Valery <nicemrlexx@live.ru>
2025-06-19 23:59:04 +0300

    bug fix

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,6 +2,8 @@ package hw10programoptimization
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"io"
 	"strings"
 
@@ -19,33 +21,65 @@ type User struct {
 	Address  string
 }
 
+var (
+	ErrBrokenEmail = errors.New("broken email")
+	ErrBrokenJSON  = errors.New("broken JSON")
+	ErrReadJSON    = errors.New("error read JSON")
+)
+
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	return countDomains(r, domain)
 }
 
-func countDomains(r io.Reader, domain string) (result DomainStat, err error) {
-	result = make(DomainStat)
+func countDomains(r io.Reader, domain string) (DomainStat, error) {
+	var user User
 
-	var (
-		user User
-		l    string
-	)
+	result := make(DomainStat)
 
 	d := "." + domain
 
 	scanner := bufio.NewScanner(r)
+
 	for scanner.Scan() {
-		l = scanner.Text()
-		if strings.Contains(l, d) {
-			if err = json.Unmarshal([]byte(l), &user); err != nil {
-				return
-			}
-			if strings.Contains(user.Email, d) {
-				result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
+		if !strings.Contains(scanner.Text(), d) {
+			continue
+		}
+
+		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
+			var jsonErr *json.SyntaxError
+			switch {
+			case errors.As(err, &jsonErr):
+				problemPart := problemJSON(scanner.Bytes(), jsonErr.Offset)
+				return nil, fmt.Errorf("error near '%s' (offset %d): %w", problemPart, jsonErr.Offset, ErrBrokenJSON)
+			default:
+				return nil, fmt.Errorf("broken json: %w", ErrBrokenJSON)
 			}
 		}
+
+		if !strings.HasSuffix(user.Email, d) {
+			continue
+		}
+
+		sp := strings.SplitN(user.Email, "@", 2)
+		if len(sp) != 2 {
+			return nil, fmt.Errorf("broken email %v: %w", user.Email, ErrBrokenEmail)
+		}
+
+		dm := strings.ToLower(sp[1])
+		result[dm]++
 	}
-	return
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error read json (%v): %w", err.Error(), ErrReadJSON)
+	}
+
+	return result, nil
+}
+
+func problemJSON(json []byte, offset int64) []byte {
+	from := min(offset, 10)
+	to := min(int64(len(json))-offset, 10)
+	return json[offset-from : offset+to]
 }
--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -6,10 +6,8 @@ package hw10programoptimization
 import (
 	"archive/zip"
 	"bytes"
-	"errors"
 	"testing"
 
-	"github.com/goccy/go-json"
 	"github.com/stretchr/testify/require"
 )
 
@@ -43,31 +41,86 @@ func TestGetDomainStat(t *testing.T) {
 }
 
 func TestBrokenJson(t *testing.T) {
-	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+
+	t.Run("error json on start", func(t *testing.T) {
+		data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5MooreTeklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{error "Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+		_, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.ErrorIs(t, err, ErrBrokenJSON)
+	})
+
+	t.Run("error json on end", func(t *testing.T) {
+		data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5MooreTeklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"error}`
+
+		_, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.ErrorIs(t, err, ErrBrokenJSON)
+	})
+
+	t.Run("find 'com', when broken json", func(t *testing.T) {
+		data := `{error"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
 {"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
 {"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
 {"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
-{error "Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
 
-	t.Run("error find 'com'", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{
+			"browsecat.com": 2,
+			"linktype.com":  1,
+		}, result)
+	})
+}
+
+func TestEmails(t *testing.T) {
+
+	t.Run("find 'com', when ....com@mail.ru", func(t *testing.T) {
+		data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"user.com@mail.ru","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{
+			"browsecat.com": 2,
+			"linktype.com":  1,
+		}, result)
+	})
+
+	t.Run("find 'com', when broken email .ru", func(t *testing.T) {
+		data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"user.commail.ru","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{
+			"browsecat.com": 2,
+			"linktype.com":  1,
+		}, result)
+	})
+
+	t.Run("find 'com', when broken email .com", func(t *testing.T) {
+		data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"user.commail.com","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
 		_, err := GetDomainStat(bytes.NewBufferString(data), "com")
-
-		var jsonErr *json.SyntaxError
-		if !errors.As(err, &jsonErr) {
-			t.Fatalf("expected SyntaxError, got %v", err)
-		}
-	})
-
-	t.Run("find 'gov'", func(t *testing.T) {
-		result, err := GetDomainStat(bytes.NewBufferString(data), "gov")
-		require.NoError(t, err)
-		require.Equal(t, DomainStat{"browsedrive.gov": 1}, result)
-	})
-
-	t.Run("find 'unknown'", func(t *testing.T) {
-		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
-		require.NoError(t, err)
-		require.Equal(t, DomainStat{}, result)
+		require.ErrorIs(t, err, ErrBrokenEmail)
 	})
 }
 
